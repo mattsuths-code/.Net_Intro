0806d064e7e3bc0ef6a8a0827f75da8069dd1833
Matt Sutherland <matt.sutherland79@gmail.com>
2019-12-16 19:50:27 -0600

    initial commit

---
+++ PizzaShop/Models/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaShop.Models
+{
+    public class Pizza : MenuItem
+    {
+        public List<string> Toppings { get; set; }
+        public string CrustType { get; set; }
+
+    }
+}
---
+++ PizzaShop/Models/PizzaFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaShop.Models
+{
+    public class PizzaFactory
+    {
+        public List<Pizza> CreatePizzas()
+        {
+            var pizzaList = new List<Pizza>();
+
+            Pizza hawaiianPizza = new Pizza();
+            hawaiianPizza.Name = "Hawaiian";
+            hawaiianPizza.Price = 17.99;
+            hawaiianPizza.Toppings = new List<string> { "canadian bacon", "pineapple" };
+            hawaiianPizza.CrustType = "thin crust";
+
+            Pizza supremePizza = new Pizza
+            {
+                Name = "Supreme",
+                Price = 19.99,
+                Toppings = new List<string> { "olives", "peppers", "pepperoni", "onions" },
+                CrustType = "regular"
+            };
+
+
+            pizzaList.Add(hawaiianPizza);
+            pizzaList.Add(supremePizza);
+
+            return pizzaList;
+        }
+    }
+}
